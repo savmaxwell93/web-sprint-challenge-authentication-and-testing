fd059371096a9fef7123e69847e41151dcf32a1a
Savannah <savmaxwell@gmail.com>
2022-04-01 13:04:11 -0600

    wip

--- api/server.test.js
+++ api/server.test.js
@@ -4,6 +4,41 @@ const request = require('supertest');
 
 const User = require('./users/users-model');
 
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+})
+beforeEach(async () => {
+  await db('users').truncate();
+})
+afterAll(async () => {
+  await db.destroy();
+})
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+test('test users-model', async () => {
+  let result;
+
+  //findAll
+  result = await User.findAll();
+  expect(result).toHaveLength(0)
+
+  //add
+  result = await User.add({ username: 'testname', password: '1234'});
+  expect(result).toEqual({ username: 'testname', password: '1234', id: 1 });
+  result = await User.findAll();
+  expect(result).toHaveLength(1);
+
+  //findById
+  result = await User.findById(2);
+  expect(result).not.toBeDefined();
+  result = await User.findById(1);
+  expect(result).toHaveProperty('username', 'testname')
+
+  //findBy
+  result = await User.findBy({ username: 'testname'})
+
 })
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,14 +1,26 @@
 const db = require('../../data/dbConfig');
 
+function findAll() {
+    return db('users').select('id', 'username')
+}
+
 function findBy(filter) {
-    return null
+    return db('users').where(filter)
+}
+
+function findById(id) {
+    return db('users').where({ id }).first()
 }
 
 async function add(user) {
-    return null
+    const [id] = await  db('users').insert(user)
+
+    return findById(id)
 }
 
 module.exports = {
+    findAll,
     findBy,
+    findById,
     add
 }
