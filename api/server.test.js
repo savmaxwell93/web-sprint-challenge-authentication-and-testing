fa40b40316f95fc2b70534067c2363d8240a1475
Savannah <savmaxwell@gmail.com>
2022-04-01 15:57:11 -0600

    completed tests for endpoints

--- api/server.test.js
+++ api/server.test.js
@@ -1,6 +1,7 @@
 const db = require('../data/dbConfig');
 const server = require('./server');
 const request = require('supertest');
+const bcrypt = require('bcryptjs');
 
 const User = require('./users/users-model');
 
@@ -41,4 +42,75 @@ test('test users-model', async () => {
   //findBy
   result = await User.findBy({ username: 'testname'})
 
+})
+describe('[POST] /api/auth/register', () => {
+  test('responds with the correct message on missing credentials', async () => {
+    let res
+    res = await request(server).post('/api/auth/register').send({ username: 'test'})
+    expect(res.status).toBe(404)
+    expect(res.body.message).toMatch(/username and password required/i)
+
+    res = await request(server).post('/api/auth/register').send({ password: 'test'})
+    expect(res.status).toBe(404)
+    expect(res.body.message).toMatch(/username and password required/i)
+  })
+  test('responds with the correct status and message when username is taken', async () => {
+    await request(server).post('/api/auth/register').send({ username: 'test', password: '1234'})
+    const res = await request(server).post('/api/auth/register').send({ username: 'test', password: '1234'})
+    expect(res.status).toBe(422)
+    expect(res.body.message).toMatch(/username taken/i)
+  })
+  test('saves the user with a bcrypted password instead of plain text', async () => {
+    await request(server).post('/api/auth/register').send({ username: 'test', password: '1234'})
+    const res = await User.findById(1)
+    expect(bcrypt.compareSync('1234', res.password)).toBeTruthy()
+    
+  })
+})
+
+describe('[POST] /api/auth/login', () => {
+  test('responds with the correct message on valid credentials', async () => {
+    await request(server).post('/api/auth/register').send({ username: 'test', password: '1234'})
+    const res = await request(server).post('/api/auth/login').send({ username: 'test', password: '1234'})
+    expect(res.body.message).toMatch(/welcome, test/i)
+  })
+  test('responds with the correct status and message on invalid credentials', async () => {
+    let res
+    await request(server).post('/api/auth/register').send({ username: 'test', password: '1234'})
+    res = await request(server).post('/api/auth/login').send({ username: 'testing', password: '1234'})
+    expect(res.body.message).toMatch(/invalid credentials/i)
+
+    res = await request(server).post('/api/auth/login').send({ username: 'test', password: '12345'})
+    expect(res.body.message).toMatch(/invalid credentials/i)
+  })
+})
+
+describe('[GET] /api/jokes', () => {
+  test('responds with the correct message on no token', async () => {
+    const res = await request(server).get('/api/jokes')
+    expect(res.body.message).toMatch(/token required/i)
+  })
+  test('responds with the correct message on invalid token', async () => {
+    const res = await request(server).get('/api/jokes').set('Authorization', 'not the right token')
+    expect(res.body.message).toMatch(/token invalid/i)
+  })
+  test('responds with list of jokes when token is valid', async () => {
+    await request(server).post('/api/auth/register').send({ username: 'test', password: '1234'})
+    let res = await request(server).post('/api/auth/login').send({ username: 'test', password: '1234'})
+    res = await request(server).get('/api/jokes').set('Authorization', res.body.token)
+    expect(res.body).toMatchObject([
+      {
+        "id": "0189hNRf2g",
+        "joke": "I'm tired of following my dreams. I'm just going to ask them where they are going and meet up with them later."
+      },
+      {
+        "id": "08EQZ8EQukb",
+        "joke": "Did you hear about the guy whose whole left side was cut off? He's all right now."
+      },
+      {
+        "id": "08xHQCdx5Ed",
+        "joke": "Why didn’t the skeleton cross the road? Because he had no guts."
+      },
+    ])
+  })
 })
